cb2a01cddff8f862448eb4c8d6b2d3b25de1f4d0
gurdeep4git <gurdeep.singh01@nagarro.com>
2021-11-19 22:36:04 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
-import logo from './logo.svg';
 import './App.css';
+import { Fragment } from 'react';
+
+const person = {
+  name: 'Adam',
+  age: 3,
+  address: {
+    city: 'Delhi'
+  }
+}
+const address = {
+  city: 'Delhi',
+  pin: 132456
+}
+
+const members = ["Steve", "John", "Will"];
+
+function formatName(name) {
+  return name + ' ' + 'Cole';
+}
 
 function App() {
+
+  const canVoteTemplate = (
+    <div>{person.age > 18 ? <p> {formatName(person.name)} can vote</p> : <p>Sorry, {formatName(person.name)} can not vote</p>}</div>
+  )
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <h3>This is for heading</h3>
+      <div className="App">
+        <p>Hello, {person.name}</p>
+        {canVoteTemplate}
+        <Welcome fname="Sara" lname="Jones" />
+        <Welcome fname="Sara 1" />
+        <Welcome fname="Sara 2" />
+        <WelcomeMembers members={members} membersCount={members.length} isActive={members.length > 1} />
+        <Address {...address} />
+        {/* <Address address={address} /> */}
+      </div>
+    </Fragment>
   );
+}
+
+function Welcome(props) {
+  const { fname, lname } = props;
+  return (
+    <Fragment>
+      <p>Welcome {fname} {lname}</p>
+    </Fragment>
+  )
+}
+
+function WelcomeMembers(props) {
+  console.log('WelcomeMembers', props);
+  return (
+    <Fragment>
+      <p>Members</p>
+    </Fragment>
+  )
+}
+
+function Address(props) {
+  const { city, pin } = props;
+  return (
+    <Fragment>
+      <p>{city}</p>
+      <p>{pin}</p>
+    </Fragment>
+  )
 }
 
 export default App;
